bab6c1f43e7d90d13e850c81c1b81d3a7f8ea3b1
rohitverma <rv16verma@gmail.com>
2023-05-15 02:25:47 +0530

    added todays learning

--- src/main/java/com/example/demo/rest/FunRestController.java
+++ src/main/java/com/example/demo/rest/FunRestController.java
@@ -1,14 +1,43 @@
 package com.example.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    //inject properties for coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    //expose new endpoint for teaminfo
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team Name: " + teamName;
+
+    }
+
+
     // expose "/" endpoint that return hello world!
     @GetMapping("/")
     public String Sayhello(){
         return "Hello World!" ;
+    }
+
+    //expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    //expose a mew endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "have a good day1";
     }
 }
